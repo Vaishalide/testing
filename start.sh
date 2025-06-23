5ed98b734b4ff1d7b9232164f0f0dc0f1deaa736
isha dalvi <dipeshxyzyadav@gmail.com>
2025-06-23 10:20:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-playwright install chromium
-gunicorn main:app --bind 0.0.0.0:$PORT
+#!/usr/bin/env bash
+# Install Chrome
+apt-get update && apt-get install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" \
+     > /etc/apt/sources.list.d/google-chrome.list
+apt-get update && apt-get install -y google-chrome-stable
+# Start Flask
+gunicorn main:app --bind 0.0.0.0:$PORT --workers 1 --timeout 120
